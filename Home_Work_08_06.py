93a7bbf7786b422afa1bc13e876cfbf2f816b565
Андрей <155187683+andreybond77@users.noreply.github.com>
2025-06-14 10:57:57 +0300

    Add files via upload

---
+++ Home_Work_08_06.py
@@ -0,0 +1,53 @@
+# def average_of_five_numbers():
+#     numbers = []
+#     for i in range(5):
+#         num = int(input(f"Введите целое число {i + 1}: "))
+#         numbers.append(num)
+#     return sum(numbers) / len(numbers)
+#
+# # Пример использования
+# result = average_of_five_numbers()
+# print("Среднее арифметическое:", result)
+
+##############################################
+
+# def count_digits():
+#     number = int(input("Введите целое число: "))
+#     return len(str(abs(number)))
+#
+# # Пример использования
+# digit_count = count_digits()
+# print("Количество цифр:", digit_count)
+
+##############################################
+
+# import math
+#
+#
+# def distance(x1, y1, x2, y2):
+#     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+#
+#
+# def triangle_perimeter():
+#     x1 = float(input("Введите координату x1: "))
+#     y1 = float(input("Введите координату y1: "))
+#     x2 = float(input("Введите координату x2: "))
+#     y2 = float(input("Введите координату y2: "))
+#     x3 = float(input("Введите координату x3: "))
+#     y3 = float(input("Введите координату y3: "))
+#
+#     side1 = distance(x1, y1, x2, y2)
+#     side2 = distance(x2, y2, x3, y3)
+#     side3 = distance(x3, y3, x1, y1)
+#
+#     return side1 + side2 + side3
+#
+#
+# # Пример использования
+# perimeter = triangle_perimeter()
+# print("Периметр треугольника:", perimeter)
+
+#########################################################
+
+
+
